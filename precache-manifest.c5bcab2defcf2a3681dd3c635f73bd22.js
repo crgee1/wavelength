a7d5a5437e829610c0eae78dc91564252bb406dc
cgee1 <christopherrgee@gmail.com>
2020-01-12 00:41:46 -0800

    Updates

---
+++ precache-manifest.c5bcab2defcf2a3681dd3c635f73bd22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af633da264d497e17ade5496e98c6d04",
+    "url": "/Wavelength/index.html"
+  },
+  {
+    "revision": "9da80a640071a2843606",
+    "url": "/Wavelength/static/css/main.b1332416.chunk.css"
+  },
+  {
+    "revision": "f9feee0a06a355fd372e",
+    "url": "/Wavelength/static/js/2.ea1c842d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Wavelength/static/js/2.ea1c842d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9da80a640071a2843606",
+    "url": "/Wavelength/static/js/main.26129311.chunk.js"
+  },
+  {
+    "revision": "7aa3f51512019f6bead2",
+    "url": "/Wavelength/static/js/runtime-main.a1894c86.js"
+  }
+]);
